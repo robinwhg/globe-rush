015a050a6d3e011a67d4cb4ee1b6d76b9129cb9d
robinwhg <mail@robinwhg.com>
2026-03-25 15:55:10 +0100

    feat: dynamically change primary color depending on the region

--- app/utils/playable-regions.ts
+++ app/utils/playable-regions.ts
@@ -4,36 +4,42 @@ export const playableRegions = [
     title: 'World',
     description: 'Play with all available countries in one run.',
     img: '/img/world.svg',
+    color: 'blue',
   },
   {
     slug: 'africa',
     title: 'Africa',
     description: 'Focus on African countries and territories.',
     img: '/img/africa.svg',
+    color: 'yellow',
   },
   {
     slug: 'americas',
     title: 'Americas',
     description: 'North, Central, South America, and the Caribbean.',
     img: '/img/americas.svg',
+    color: 'green',
   },
   {
     slug: 'asia',
     title: 'Asia',
     description: 'Play countries across Western to Eastern Asia.',
     img: '/img/asia.svg',
+    color: 'orange',
   },
   {
     slug: 'europe',
     title: 'Europe',
     description: 'Challenge yourself with European flags.',
     img: '/img/europe.svg',
+    color: 'red',
   },
   {
     slug: 'oceania',
     title: 'Oceania',
     description: 'Includes Australia, Polynesia, Micronesia, and Melanesia.',
     img: '/img/oceania.svg',
+    color: 'pink',
   },
 ] as const
 
