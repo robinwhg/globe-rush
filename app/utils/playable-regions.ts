2d86064cafbd2da24b6d7ca95df28110e4e5870e
robinwhg <mail@robinwhg.com>
2026-04-13 11:12:41 +0200

    feat: add custom games to regions

--- app/utils/playable-regions.ts
+++ app/utils/playable-regions.ts
@@ -5,6 +5,11 @@ export const playableRegions = [
     description: 'A full run with flags from every region around the globe.',
     img: '/img/world.svg',
     color: 'blue',
+    games: [{
+      slug: 'all-countries-territories',
+      title: 'All countries and territories of the World',
+      countries,
+    }],
   },
   {
     slug: 'africa',
@@ -12,6 +17,11 @@ export const playableRegions = [
     description: 'Bold palettes and several flags that are easy to mix-up.',
     img: '/img/africa.svg',
     color: 'yellow',
+    games: [{
+      slug: 'all-countries-territories',
+      title: 'All countries and territories of Africa',
+      countries: countries.filter(country => country.region === 'Africa'),
+    }],
   },
   {
     slug: 'americas',
@@ -19,6 +29,11 @@ export const playableRegions = [
     description: 'North, Central, South America and the Caribbean.',
     img: '/img/americas.svg',
     color: 'green',
+    games: [{
+      slug: 'all-countries-territories',
+      title: 'All countries and territories of the Americas',
+      countries: countries.filter(country => country.region === 'Americas'),
+    }],
   },
   {
     slug: 'asia',
@@ -26,6 +41,11 @@ export const playableRegions = [
     description: 'Wide stylistic range, from simple tricolors to ornate emblems.',
     img: '/img/asia.svg',
     color: 'orange',
+    games: [{
+      slug: 'all-countries-territories',
+      title: 'All countries and territories of Asia',
+      countries: countries.filter(country => country.region === 'Asia'),
+    }],
   },
   {
     slug: 'europe',
@@ -33,6 +53,11 @@ export const playableRegions = [
     description: 'Many closely related patterns that reward careful comparison.',
     img: '/img/europe.svg',
     color: 'red',
+    games: [{
+      slug: 'all-countries-territories',
+      title: 'All countries and territories of Europe',
+      countries: countries.filter(country => country.region === 'Europe'),
+    }],
   },
   {
     slug: 'oceania',
@@ -40,5 +65,10 @@ export const playableRegions = [
     description: 'Australia, New Zealand, Melanesia, Micronesia, and Polynesia.',
     img: '/img/oceania.svg',
     color: 'pink',
+    games: [{
+      slug: 'all-countries-territories',
+      title: 'All countries and territories of Oceania',
+      countries: countries.filter(country => country.region === 'Oceania'),
+    }],
   },
 ] as const
